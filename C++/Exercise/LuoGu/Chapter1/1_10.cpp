24022d78988a80c090afa9f49fc516f478e80fa1
Zhai Yixiao <40822014+smilida@users.noreply.github.com>
2023-05-08 10:58:48 +0800

    Create 1_10.cpp

---
+++ C++/Exercise/LuoGu/Chapter1/1_10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int num = 1;              // Day 4
+    num = (num + 1) * 2;      // Day 3
+    num = (num + 1) * 2;      // Day 2
+    num = (num + 1) * 2;      // Day 1
+    std::cout << num << std::endl;
+    return 0;
+}
