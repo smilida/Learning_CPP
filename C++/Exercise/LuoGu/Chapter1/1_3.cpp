e7b5a06d066ef7ce47c538c37d6455228dc63c7a
Zhai Yixiao <40822014+smilida@users.noreply.github.com>
2023-05-08 10:24:29 +0800

    Create 1_3.cpp

---
+++ C++/Exercise/LuoGu/Chapter1/1_3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << 14 / 4 << std::endl;
+    std::cout << 14 / 4 * 4 << std::endl;
+    std::cout << 14 - 14 / 4 * 4 << std::endl;
+//    std::cout << 14 % 4 << std::endl;
+    return 0;
+}
