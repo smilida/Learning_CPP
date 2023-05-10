ffc704be1c2c76c52c537662d9abd35a42bbc2dc
Zhai Yixiao <40822014+smilida@users.noreply.github.com>
2023-05-10 11:30:03 +0800

    Create 2_2.cpp

---
+++ C++/Exercise/LuoGu/Chapter2/2_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int ans1;
+    char ans2;
+    ans1 = 'M' - 'A' + 1;
+    ans2 = 'A' + 18 - 1;
+    std::cout << ans1 << std::endl;
+    std::cout << ans2 << std::endl;
+}
