d7df81e50744d5544f46f9e5cac1587c3e2181a7
Zhai Yixiao <40822014+smilida@users.noreply.github.com>
2023-05-13 13:12:47 +0800

    Update 3_4.cpp

--- C++/Exercise/LuoGu/Chapter3/3_4.cpp
+++ C++/Exercise/LuoGu/Chapter3/3_4.cpp
@@ -5,9 +5,8 @@ int main() {
     std::cin >> x;
     std::cout << "Today, I ate " << x << " apple";
     if (x != 0 && x != 1) {
-        std::cout << "s." << std::endl;
-    } else {
-        std::cout << "." << std::endl;
+        std::cout << "s" ;
     }
+    std::cout << "." << std::endl;
     return 0;
 }
